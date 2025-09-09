5690bc7a18ee3c366eab4f070b6b23d2eb3fdddd
TacitusVanWinklie <max.maempel@gmail.com>
2025-09-09 12:48:09 +0200

    ETF Aufgabe 1

--- Schule/src/blatt04/ETF.java
+++ Schule/src/blatt04/ETF.java
@@ -1,22 +1,33 @@
 package blatt04;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class ETF {
 
     public static void main(String[] args) {
+        Random rnd = new Random();
 
-        double sparbetrag = 50;
-        double wertsteigung = 1.06;
+        double sparbetrag = 0;
+        double zinssatz = rnd.nextDouble();
         double wert =  3578.89;
         int monat = 0;
+        int jahr = 0;
 
-        while(monat <= 12){
-            double neuerWert = wertsteigung * sparbetrag;
-            monat = monat + 1;
+        zinssatz = zinssatz * 0.06;
+        zinssatz = zinssatz + 1.06;
 
+        while (jahr < 5){
 
+            while(monat < 12){
+                sparbetrag += 50;
+                double neuerWert = zinssatz * sparbetrag;
+                monat = monat + 1;
+                System.out.println("Jahr: " + jahr + ", Monat: " + monat + "," + " Neuer Wert: " + neuerWert);
+            }
+            monat = 0;
+            jahr++;
         }
+
 
     }
 
