1dae898459c3934643043c44b0ccae0784cd7760
TacitusVanWinkle <max.maempel@gmail.com>
2025-09-13 09:23:05 +0200

    Potenzierung Fertig

--- Schule/src/blatt04/Poternzierung.java
+++ Schule/src/blatt04/Poternzierung.java
@@ -14,17 +14,17 @@ public class Poternzierung {
         double ergebnis = 1;
         int variable = 0;
 
-        if (exponent >= 0) {
-            while (variable < exponent) {
-                ergebnis = ergebnis * zahl;
-                variable++;
+        if (exponent >= 0) {    //Wenn der Exponent größer oder gleich null ist wird die erste while Schleife ausgeführt.
+            while (variable < exponent) {   //Diese wird solange ausgeführt wie variable (0) kleiner als der Exponent ist.
+                ergebnis = ergebnis * zahl; //Der int ergebnis wird mit sich selbst genommen.
+                variable++; //Wenn die Schleife durch ist wird variable 1 zugefügt. Das sorgt für einen Stopp der Schleife nach so vielen Durchgängen wie Zahl des Exponenten.
             }
-        } else if (exponent < 0) {
-            while (variable > exponent) {
-                ergebnis = ergebnis * zahl;
-                variable--;
+        } else if (exponent < 0) {  //Wenn der Exponent kleiner als null ist wird die zweite while Schleife durchgeführt.
+            while (variable > exponent) {   //Diese wird solange durchgeführt wie variable (0) größer als der Exponent ist.
+                ergebnis = ergebnis * zahl; //Gleiche Rechnung wie oben.
+                variable--; //Wenn die Schleife durch ist wird variable 1 abgezogen. Somit stoppt die Schleife sobald variable gleich Exponent ist.
             }
-            ergebnis = 1 / ergebnis;
+            ergebnis = 1 / ergebnis;    //Da negative Potenzen für 0,[...] ergebnise sorgen muss 1 durch das Ergebnis der vorherigen Rechnung genommen werden.
         }
         System.out.println(zahl + "^" + exponent + " = " + ergebnis);
         }
