5ec30091706b28c7e820f09e2a8e5cdb7b27f071
TacitusVanWinkle <max.maempel@gmail.com>
2025-09-14 22:15:00 +0200

    Blatt 04 komplett fertig!!! (diesmal richtig)

--- Schule/src/blatt04/ETF.java
+++ Schule/src/blatt04/ETF.java
@@ -9,17 +9,27 @@ public class ETF {
         Random rnd = new Random();
         Scanner input = new Scanner(System.in);
 
-        double sparbetrag = 0;
         double wert =  3578.89;
         int monat = 0;
-        int jahr = 0;
+        int jahr = 1;
 
         System.out.println("Wie viele Jahre willst du berechnen?");
         int jahre = input.nextInt();
-        while (jahr < jahre){
+
+        if (jahre < 0){
+            System.out.println("Du darfst keine negativen Zahlen eingeben!");
+            System.exit(0);
+        }
+
+        while (true){
+            if (jahr > jahre){
+                break;
+            }
+
+            System.out.println("Wie viel möchtest du dieses Jahr pro Monat investieren?");
+            double sparbetrag = input.nextDouble();
 
             while(monat < 12){
-                sparbetrag += 50;
                 double zinssatz = rnd.nextDouble();
                 zinssatz = zinssatz * 0.06;
                 zinssatz = zinssatz + 1.06;
