e46cc40f79f57ed4bc86c97145bef21efdab8565
TacitusVanWinklie <max.maempel@gmail.com>
2025-09-17 11:18:32 +0200

    Flussdiagramm

---
+++ Schule/src/blatt05/Flussdiagramm.java
@@ -0,0 +1,9 @@
+package blatt05;
+
+import java.util.Scanner;
+
+public class Flussdiagramm {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
