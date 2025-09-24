7260649675208e0ffdec394d77e08e4ee21e5516
TacitusVanWinklie <max.maempel@gmail.com>
2025-09-24 11:20:18 +0200

    Wertetabelle bitte hilf mir wtf

---
+++ Schule/src/blatt06/Wertetabelle.java
@@ -0,0 +1,15 @@
+package blatt06;
+
+import java.util.Scanner;
+
+public class Wertetabelle {
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int grad = input.nextInt();
+        double[] coeff = new double[grad + 1];
+        for (int a = 0; a < coeff.length; a++) {
+
+        }
+    }
+}
