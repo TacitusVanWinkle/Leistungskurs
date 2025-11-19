de27ae8f26cf23a014ea9463e7570ddd6bf55d94
MaxMaempel <max.maempel@gmail.com>
2025-11-19 10:33:53 +0100

    a

--- Schule/src/blatt13/Zufall.java
+++ Schule/src/blatt13/Zufall.java
@@ -3,19 +3,19 @@ package blatt13;
 public class Zufall {
 
     public static double zufall(int b) {
-        return Math.random() * b + 1;
+        return (Math.random() * b);
     }
 
     public static int zufallGanz(int b) {
-        return (int) Math.random() * b + 1;
+        return (int) (Math.random() * b);
     }
 
     public static double zufall(int a, int b) {
-        return Math.random() * b + a;
+        return (Math.random() * b + a);
     }
 
     public static int zufallGanz(int a, int b) {
-        return (int) Math.random() * b + a;
+        return (int) (Math.random() * b + a);
     }
 
     public static int[] zufallArray(int l, int a, int b) {
@@ -27,7 +27,9 @@ public class Zufall {
     }
 
     public static void main(String[] args) {
+        System.out.println(zufallGanz(5));
 
+        System.out.println(zufall(0, 5));
     }
 
 }
