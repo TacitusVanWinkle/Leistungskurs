6947e0b1af8e1b0061408adf5711c759388b9e45
TacitusVanWinkle <max.maempel@gmail.com>
2025-11-23 13:34:57 +0100

    Stein fertig

--- Schule/src/blatt07/Stein.java
+++ Schule/src/blatt07/Stein.java
@@ -1,34 +1,43 @@
-/*package blatt07;
-
-import java.util.Scanner;
+package blatt07;
 
 public class Stein {
 
+    public static int steinGGT(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
 
- public static int steinGGT(int a, int b) {
+        int k = 0;
 
-     while (a != 0 || b != 0) {
-         if (a % 2 == 0 && b % 2 == 0) {
-             a /= 2;
-             b /= 2;
-         } else if (a % 2 == 0 ) {
-             a /= 2;
+        while (a != 0 && b != 0) {
 
-         }else if (b % 2 == 0) {
-             b /= 2;
-         }
+            if (a % 2 == 0 && b % 2 == 0) {
+                a /= 2;
+                b /= 2;
+                k++;
+            }
 
-     } else {
-         if ()
-     }
- }
+            else if (a % 2 == 0) {
+                a /= 2;
+            }
+            else if (b % 2 == 0) {
+                b /= 2;
+            }
 
- public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
+            else {
+                if (a > b) {
+                    a = (a - b) / 2;
+                } else {
+                    b = (b - a) / 2;
+                }
+            }
+        }
 
-     int a = sc.nextInt();
-     int b = sc.nextInt();
+        int rest = (a == 0) ? b : a;
 
- }
+        return rest * (int)Math.pow(2, k);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(steinGGT(48, 18)); // sollte 6 ergeben
+    }
 }
-*/
