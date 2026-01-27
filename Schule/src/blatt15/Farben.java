307ce7002b8ca5103d8fc2ad5776051b7409cc5f
MaxMaempel <max.maempel@gmail.com>
2026-01-27 13:03:26 +0100

    Vorbereitne

--- Schule/src/blatt15/Farben.java
+++ Schule/src/blatt15/Farben.java
@@ -10,14 +10,22 @@ public class Farben {
     static char[][] spielfeld;
 
     public static void initialisiereSpielfeld () {
-        spielfeld = MultiArrays.createEmpty2DCharArray(80, 80);
-        for (int i = 0; i < 80; i++) {
+        spielfeld = MultiArrays.createEmpty2DCharArray(40, 40);
+        for (int i = 0; i < 40; i++) {
                 spielfeld[0][i] = '8';
-                spielfeld[79][i] = '8';
+                spielfeld[39][i] = '8';
                 spielfeld[i][0] = '8';
-                spielfeld[i][79] = '8';
+                spielfeld[i][39] = '8';
             }
         }
+
+    public static void startPositionen () {
+        for (int i = spielfeld.length - 1; i >= 0; i--) {
+            if (i = spielfeld.length / 2) {
+
+            }
+        }
+    }
 
     public static void main(String[] args) {
         SchischVisualizer sv = new SchischVisualizer();
